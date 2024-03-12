dd3767c28feef9406203eb704062cbb0024e755e
bechir mahamat <bechirmahamat11@gmail.com>
2024-03-12 20:09:42 +0300

    first commit

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,4 +1,22 @@
 const About = () => {
-    return <h1 className="text-3xl text-secondary">About</h1>;
-};
-export default About;
+    return (
+        <div class='container mx-auto px-4 py-8'>
+            <h2 class='text-2xl font-semibold mb-4'>About Us</h2>
+            <p class='color- leading-relaxed'>
+                Welcome to Comfy Store! We're dedicated to bringing you the most
+                comfortable and stylish products for your home. Whether you're
+                looking for cozy furniture, soft blankets, or plush cushions,
+                we've got you covered. Our mission is to make your home a haven
+                of comfort and relaxation.
+            </p>
+            <p class='color-primary leading-relaxed mt-4'>
+                At Comfy Store, we believe that everyone deserves to feel
+                comfortable in their own space. That's why we carefully curate
+                our collection to offer high-quality products at affordable
+                prices. Customer satisfaction is our top priority, and we're
+                always here to help you find the perfect pieces for your home.
+            </p>
+        </div>
+    )
+}
+export default About
